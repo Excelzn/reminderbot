387622b12306d4df0e708929621b4ce1245c6ce0
excel <excelzn91@gmail.com>
2024-09-29 12:54:33 -0400

    adding other files

---
+++ ReminderBot/Migrations/20240929154116_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ReminderBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reminders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReminderText = table.Column<string>(type: "text", nullable: false),
+                    Channel = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    RolesToPing = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    UsersToPing = table.Column<decimal[]>(type: "numeric(20,0)[]", nullable: false),
+                    RemindAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reminders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reminders");
+        }
+    }
+}
---
+++ ReminderBot/Migrations/20240929154534_UsersNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReminderBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal[]>(
+                name: "UsersToPing",
+                table: "Reminders",
+                type: "numeric(20,0)[]",
+                nullable: true,
+                oldClrType: typeof(decimal[]),
+                oldType: "numeric(20,0)[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal[]>(
+                name: "UsersToPing",
+                table: "Reminders",
+                type: "numeric(20,0)[]",
+                nullable: false,
+                defaultValue: new decimal[0],
+                oldClrType: typeof(decimal[]),
+                oldType: "numeric(20,0)[]",
+                oldNullable: true);
+        }
+    }
+}
